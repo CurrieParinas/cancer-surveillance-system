c1b66ca1e9882177d054f80b9ea28d1688e392be
Currie Exekiel Pariñas <85160106+CurrieParinas@users.noreply.github.com>
2024-09-12 22:43:53 +0800

    [Feat][Currie] - added layout sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,14 @@
+import { Sidebar } from "@/components/atoms/sidebar";
+
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-        <>
-          {children}
-        </>
-    );
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex screen">
+      <Sidebar />
+      {children}
+    </div>
+  );
+}
